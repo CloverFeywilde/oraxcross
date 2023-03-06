00973bf2208e3d07161bec6148a83358e1bfe0e7
Clover Feywilde <clover.feywilde@gmail.com>
2023-03-05 19:27:09 -0500

    added fieldcontroller

---
+++ FieldController.js
@@ -0,0 +1,24 @@
+/*
+    added by Clover
+    --Field Class for tracking field state--
+*/ 
+
+class field {
+    deck = [];
+    refDeck = [];
+    discard = [];
+    facet1 = '';
+
+    moveCards(source, destination, numOfCards){
+    }
+  }
+
+
+/*
+let player = new field();
+
+let player2 = new field();
+*/
+
+
+  
