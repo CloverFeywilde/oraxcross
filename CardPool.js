e6c09f6de00bfeff6e427446f8421b1a7b47c231
wolveseur <dragonfire62702@gmail.com>
2023-03-05 18:57:33 -0700

    rei and aya cards 1-4

--- CardPool.js
+++ CardPool.js
@@ -1,4 +1,129 @@
 /* 
     File created by Nox
-*/
 
+    Basic Templates
+
+    Reflection:
+            'abc001' : {
+        'title' : 'Title Here',
+        'text' : 'Text Placeholder',
+        'type' : 'Reflection',
+        'art' : 'artlink',
+        'attributes' : {
+            'rank' : 'S, A, B, C',
+            'element' : 'Blanc, Noir, Terra',
+        },
+    },
+
+    Facet:
+        'aya001' : {
+        'title' : 'Title Here',
+        'text' : 'Text Placeholder',
+        'type' : 'Facet',
+        'art' : 'artlink',
+        'attributes' : {
+            'rank' : '1, 2, 3, 4',
+            'element' : 'Blanc, Noir, Terra',
+        },
+    },
+
+    Hex: 
+        'aya001' : {
+        'title' : 'Title Here',
+        'text' : 'Text Placeholder',
+        'type' : 'Hex',
+        'art' : 'artlink',
+        'attributes' : {
+            'element' : 'Blanc, Noir, Terra',
+        },
+    },
+
+    Charm:
+        'aya001' : {
+        'title' : 'Title Here',
+        'text' : 'Text Placeholder',
+        'type' : 'Charm',
+        'art' : 'artlink',
+        'attributes' : {
+            'element' : 'Blanc, Noir, Terra Acqua',
+        },
+    },
+*/
+const cardPool = {
+    'aya001' : {
+        'title' : 'Ayami Prime',
+        'text' : 'Text Placeholder',
+        'type' : 'Reflection',
+        'art' : 'artlink',
+        'attributes' : {
+            'rank' : 'S',
+            'element' : 'Acqua',
+        },
+    },
+    'aya002' : {
+    'title' : 'Kanna Minami',
+    'text' : 'Text Placeholder',
+    'type' : 'Facet',
+    'art' : 'artlink',
+    'attributes' : {
+            'rank' : '4',
+            'element' : 'Blanc',
+        },
+    },
+    'aya003' : {
+    'title' : 'Siren Call',
+    'text' : 'Text Placeholder',
+    'type' : 'Hex',
+    'art' : 'artlink',
+    'attributes' : {
+            'element' : 'Acqua',
+        },
+    },
+    'aya004' : {
+    'title' : 'Ika-Chan',
+    'text' : 'Text Placeholder',
+    'type' : 'Charm',
+    'art' : 'artlink',
+    'attributes' : {
+            'element' : 'Acqua',
+        },
+    },
+    'rei001' : {
+        'title' : 'Miriko, Child of Gaia',
+        'text' : 'Text Placeholder',
+        'type' : 'Reflection',
+        'art' : 'artlink',
+        'attributes' : {
+            'rank' : 'S',
+            'element' : 'Blanc',
+        },
+    },
+    'rei002' : {
+        'title' : 'Rei, Resistance Leader',
+        'text' : 'Text Placeholder',
+        'type' : 'Facet',
+        'art' : 'artlink',
+        'attributes' : {
+            'rank' : '4',
+            'element' : 'Noir',
+        },
+    },
+    'rei003' : {
+        'title' : 'New Identity',
+        'text' : 'Text Placeholder',
+        'type' : 'Hex',
+        'art' : 'artlink',
+        'attributes' : {
+            'element' : 'Noir',
+        },
+    },
+    'rei004' : {
+        'title' : 'Reflections of the Past',
+        'text' : 'Text Placeholder',
+        'type' : 'Charm',
+        'art' : 'artlink',
+        'attributes' : {
+            'element' : 'Blanc',
+        },
+    },
+};
